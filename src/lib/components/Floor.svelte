25d1e599906fc7fc512a7bce9a0a1d3710152835
Ted Waller <ted.waller.iii@gmail.com>
2023-11-20 11:15:05 -0500

    fix floor

--- src/lib/components/Floor.svelte
+++ src/lib/components/Floor.svelte
@@ -20,14 +20,14 @@
   .floor {
     overflow: hidden;
     position: absolute;
-    width: 200%;
-    left: -60%;
-    height: 130%;
-    bottom: -30%;
+    width: 140%;
+    left: -20%;
+    height: 80%;
+    bottom: -0%;
     background-image:
-      linear-gradient(to bottom, #6eccee 4px, transparent 4px),
-      linear-gradient(to left, #6eccee 4px, transparent 4px);
-    background-size: 50px 50px, 100px 100px;
+      linear-gradient(to bottom, #6eccee 2px, transparent 2px),
+      linear-gradient(to left, #6eccee 2px, transparent 2px);
+    background-size: 20px 20px, 50px 50px;
     background-position: -1px -1px, -1px -1px;
     transform: rotateX(75deg);
     animation: moveUp 5s infinite linear;
@@ -37,7 +37,7 @@
       position: absolute;
       width: 100%;
       height: 100%;
-      background: linear-gradient(to bottom,  rgb(20, 12, 30) 20%, transparent);
+      background: linear-gradient(to bottom,  rgb(20, 12, 30) 40%, transparent 70%);
     }
   }
 
@@ -46,7 +46,7 @@
     background-position: 0px 0px, -1px -1px;
   }
   100% {
-    background-position: -50px -50px, -1px -1px;
+    background-position: -20px -20px, -1px -1px;
   }
 }
 </style>
